f147bf82b801aa1d0d7ad79bb84c9d8c4556f790
KisoGit <maximilian.ackermann@gmx.de>
2019-12-10 12:24:53 +0100

    added cout functionality.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main() {
     
-    cout <<"Hello world!" << endl;
+    string hello = "Hello World";
+    int number;
+    number = 13;
+    cout << "what do you call this type of program?\n" << hello << " \n Oh I remember..." << endl;
 
     return 0;
 }
